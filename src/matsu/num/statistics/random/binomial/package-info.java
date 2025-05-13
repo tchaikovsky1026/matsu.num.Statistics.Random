a2534deac34e99bbae2901904cd7bbed8dd985d9
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-05-13 10:50:57 +0900

    add: BinomialRndの骨格実装を用意した.

---
+++ src/matsu/num/statistics/random/binomial/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+/**
+ * 二項分布の乱数発生器の実装を扱う.
+ */
+package matsu.num.statistics.random.binomial;
