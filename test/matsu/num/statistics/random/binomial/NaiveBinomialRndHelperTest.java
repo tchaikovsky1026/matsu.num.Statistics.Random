ecff7868d1f132d820073e1cc12a156c5258e4b2
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-05-22 15:42:20 +0900

    add,test; 素朴な二項乱数のヘルパ

---
+++ test/matsu/num/statistics/random/binomial/NaiveBinomialRndHelperTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+package matsu.num.statistics.random.binomial;
+
+import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import matsu.num.statistics.random.BaseRandom;
+import matsu.num.statistics.random.BinomialRnd;
+import matsu.num.statistics.random.IntegerRandomGeneratorTestingFramework;
+
+/**
+ * {@link NaiveBinomialRndHelper} クラスのテスト.
+ */
+@RunWith(Enclosed.class)
+final class NaiveBinomialRndHelperTest {
+
+    private static final BinomialRnd.Factory FACTORY = new Factory();
+
+    @RunWith(Theories.class)
+    public static class パラメータによる網羅的テスト {
+
+        @DataPoints
+        public static BinomialParameters[] parameters = {
+                new BinomialParameters(10, 0.1),
+                new BinomialParameters(31, 0.1),
+                new BinomialParameters(100, 0.1),
+                new BinomialParameters(10, 0.3),
+                new BinomialParameters(31, 0.3),
+                new BinomialParameters(100, 0.3),
+                new BinomialParameters(10, 0.7),
+                new BinomialParameters(31, 0.7),
+                new BinomialParameters(100, 0.7)
+        };
+
+        @Theory
+        public void test(BinomialParameters parameter) {
+            IntegerRandomGeneratorTestingFramework framework =
+                    IntegerRandomGeneratorTestingFramework.instanceOf(
+                            new TestedBinomialRandomGenerator(parameter.createFrom(FACTORY)));
+            framework.test();
+        }
+    }
+
+    private static final class Factory extends SkeletalBinomialRnd.Factory {
+
+        @Override
+        BinomialRnd createInstanceOf(int n, double p) {
+            return new BinomialImpl(n, p);
+        }
+
+        private static final class BinomialImpl extends SkeletalBinomialRnd {
+
+            private final NaiveBinomialRndHelper helper;
+
+            BinomialImpl(int n, double p) {
+                super(n, p);
+                this.helper = new NaiveBinomialRndHelper();
+            }
+
+            @Override
+            public int nextRandom(BaseRandom random) {
+                return this.helper.next(this.n, this.p, random);
+            }
+        }
+    }
+}
