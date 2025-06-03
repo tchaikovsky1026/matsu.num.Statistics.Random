3d0065bef1e3c68169383a2ffc9e4e8c582b2a0f
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-06-03 10:25:05 +0900

    feat,test: RejectionSamplingZetaRndの整備とテスト

---
+++ src/matsu/num/statistics/random/zeta/RejectionSamplingZetaRnd.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/*
+ * 2025.6.3
+ */
+package matsu.num.statistics.random.zeta;
+
+import java.util.Objects;
+
+import matsu.num.statistics.random.BaseRandom;
+import matsu.num.statistics.random.ExponentialRnd;
+import matsu.num.statistics.random.ZetaRnd;
+import matsu.num.statistics.random.lib.Exponentiation;
+
+/**
+ * <p>
+ * 棄却法によるゼータ乱数発生器を扱う.
+ * </p>
+ * 
+ * <p>
+ * TODO: アルゴリズムに関するドキュメントの整備.
+ * </p>
+ * 
+ * @author Matsuura Y.
+ */
+public final class RejectionSamplingZetaRnd extends SkeletalZetaRnd {
+
+    private static final double LN_0_5 = Math.log(0.5);
+
+    private final Exponentiation exponentiation;
+    private final ExponentialRnd exponentialRnd;
+
+    private final double s_minus_1;
+    private final double inv_s_minus_1;
+
+    private final double c;
+
+    /**
+     * 公開されないコンストラクタ. <br>
+     * 引数チェックは行われていないので注意.
+     */
+    private RejectionSamplingZetaRnd(double s,
+            Exponentiation exponentiation,
+            ExponentialRnd.Factory exponentialRndFactory) {
+        super(s);
+        this.exponentiation = exponentiation;
+        this.exponentialRnd = exponentialRndFactory.instance();
+
+        this.s_minus_1 = s - 1;
+        this.inv_s_minus_1 = 1d / this.s_minus_1;
+        this.c = 1 - exponentiation.exp(LN_0_5 * s_minus_1);
+    }
+
+    @Override
+    public int nextRandom(BaseRandom random) {
+        while (true) {
+            double z = exponentiation.exp(exponentialRnd.nextRandom(random) * inv_s_minus_1);
+            int y = !(z <= Integer.MAX_VALUE)
+                    ? 1
+                    : (int) z;
+
+            double w = 1d / y;
+            double cw = c * w;
+
+            double u = random.nextDouble();
+
+            if (u <= cw ||
+                    u * (1 - exponentiation.exp(-s_minus_1 * exponentiation.log(1d + w))) <= cw) {
+                return y;
+            }
+        }
+    }
+
+    /**
+     * {@link matsu.num.statistics.random.ZetaRnd.Factory} を生成する.
+     * 
+     * @param exponentiation 指数関数の計算
+     * @param exponentialRndFactory 指数乱数発生器のファクトリ
+     * @return ゼータ乱数のファクトリ
+     * @throws NullPointerException 引数がnullの場合
+     */
+    public static ZetaRnd.Factory createFactory(Exponentiation exponentiation,
+            ExponentialRnd.Factory exponentialRndFactory) {
+        return new Factory(
+                Objects.requireNonNull(exponentiation),
+                Objects.requireNonNull(exponentialRndFactory));
+    }
+
+    private static final class Factory extends SkeletalZetaRnd.Factory {
+
+        private final Exponentiation exponentiation;
+        private final ExponentialRnd.Factory exponentialRndFactory;
+
+        /**
+         * 非公開のコンストラクタ. <br>
+         * 生成はstaticメソッドにより行われる.
+         */
+        Factory(Exponentiation exponentiation, ExponentialRnd.Factory exponentialRndFactory) {
+            super();
+            this.exponentiation = exponentiation;
+            this.exponentialRndFactory = exponentialRndFactory;
+        }
+
+        @Override
+        ZetaRnd createInstanceOf(double s) {
+            return new RejectionSamplingZetaRnd(s, exponentiation, exponentialRndFactory);
+        }
+    }
+}
---
+++ test/matsu/num/statistics/random/zeta/RejectionSamplingZetaRndTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+package matsu.num.statistics.random.zeta;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import matsu.num.statistics.random.IntegerRandomGeneratorTestingFramework;
+import matsu.num.statistics.random.ZetaRnd;
+import matsu.num.statistics.random.exp.ExponentialFactoryForTesting;
+import matsu.num.statistics.random.lib.ExponentiationForTesting;
+
+/**
+ * {@link RejectionSamplingZetaRnd} のテスト.
+ */
+@RunWith(Enclosed.class)
+final class RejectionSamplingZetaRndTest {
+
+    public static final Class<?> TEST_CLASS = RejectionSamplingZetaRnd.class;
+
+    private static final ZetaRnd.Factory FACTORY =
+            RejectionSamplingZetaRnd.createFactory(
+                    ExponentiationForTesting.INSTANCE,
+                    ExponentialFactoryForTesting.FACTORY);
+
+    @RunWith(Theories.class)
+    public static class 乱数のテスト {
+
+        @DataPoints
+        public static double[] s_values = { 2, 3, 4, 10 };
+
+        @Theory
+        public void test_s_2(double s) {
+            IntegerRandomGeneratorTestingFramework framework =
+                    IntegerRandomGeneratorTestingFramework.instanceOf(
+                            new TestedZetaRandomGenerator(FACTORY.instanceOf(s)));
+            framework.test();
+        }
+    }
+
+    public static class toString表示 {
+
+        @Test
+        public void test_toString() {
+            System.out.println(TEST_CLASS.getName());
+            System.out.println(FACTORY);
+            System.out.println(FACTORY.instanceOf(2));
+            System.out.println();
+        }
+    }
+}
