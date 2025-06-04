a3ca4ab6e0ffae5323cbbc43a2e288067c59e18e
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-06-05 08:20:05 +0900

    refactor: ZigguratExpRndの内部について, 内部のデータ構造の最適化

--- src/matsu/num/statistics/random/exp/LongSubstitutedZiggExponentialRnd.java
+++ src/matsu/num/statistics/random/exp/LongSubstitutedZiggExponentialRnd.java
@@ -33,7 +33,10 @@ public final class LongSubstitutedZiggExponentialRnd extends SkeletalExponential
 
     private static final double LONG_TO_DOUBLE_COEFF = 1d / (1L << (64 - N_BIT));
 
-    private final double[] xi;
+    /**
+     * xi と (xi * LONG_TO_DOUBLE_COEFF) を扱う.
+     */
+    private final double[] xi_and_xiTimesCoef;
     private final double[] fi;
 
     private final Exponentiation exponentiation;
@@ -47,17 +50,22 @@ public final class LongSubstitutedZiggExponentialRnd extends SkeletalExponential
         super();
         this.exponentiation = Objects.requireNonNull(exponentiation);
 
-        xi = new double[N + 1];
+        xi_and_xiTimesCoef = new double[2 * N + 2];
         fi = new double[N + 1]; //配列サイズが2^n個にならないよう、無駄スペースあり
-        xi[N] = V_N / func(R_N);
-        xi[N - 1] = R_N;
-        fi[N - 1] = func(xi[N - 1]);
+
+        setXi(N, V_N / func(R_N));
+        setXi(N - 1, R_N);
+        fi[N - 1] = func(getXi(N - 1));
         for (int i = N - 2; i >= 1; i--) {
-            xi[i] = funcInv(fi[i + 1] + V_N / xi[i + 1]);
-            fi[i] = func(xi[i]);
+            setXi(i, funcInv(fi[i + 1] + V_N / getXi(i + 1)));
+            fi[i] = func(getXi(i));
         }
-        xi[0] = 0.0;
-        fi[0] = func(xi[0]);
+        setXi(0, 0.0);
+        fi[0] = func(getXi(0));
+
+        for (int i = 0; i <= N; i++) {
+            calcAndSetXiTimesCoeff(i, getXi(i));
+        }
     }
 
     @Override
@@ -68,15 +76,16 @@ public final class LongSubstitutedZiggExponentialRnd extends SkeletalExponential
         while (true) {
             /*
              * 処理のメインとなる randomDouble を long で代用する.
-             * u がdouble値に相当する.
+             * u = LONG_TO_DOUBLE_COEFF * (long64 >>> N_BIT)
+             * がdouble値に相当する.
              */
             long long64 = random.nextLong();
             int iArea = (int) (long64 & (N - 1));
 
-            // TODO: この部分は, 直接xを計算できるように修正可能.
-            double u = LONG_TO_DOUBLE_COEFF * (long64 >>> N_BIT);
-            double x = xi[iArea + 1] * u;
-            if (x < xi[iArea]) {
+            double xiTimesCoeff_at_iAreaPlus1 = getXiTimesCoeff(iArea + 1);
+            double xi_at_iArea = getXi(iArea);
+            double x = xiTimesCoeff_at_iAreaPlus1 * (long64 >>> N_BIT);
+            if (x < xi_at_iArea) {
                 return x + x0;
             }
             if (iArea == N - 1) {
@@ -101,6 +110,22 @@ public final class LongSubstitutedZiggExponentialRnd extends SkeletalExponential
      */
     private double funcInv(double f) {
         return -exponentiation.log(f);
+    }
+
+    private double getXi(int i) {
+        return xi_and_xiTimesCoef[i << 1];
+    }
+
+    private double getXiTimesCoeff(int i) {
+        return xi_and_xiTimesCoef[(i << 1) | 1];
+    }
+
+    private void setXi(int i, double xiValue) {
+        xi_and_xiTimesCoef[i << 1] = xiValue;
+    }
+
+    private void calcAndSetXiTimesCoeff(int i, double xiValue) {
+        xi_and_xiTimesCoef[(i << 1) | 1] = xiValue * LONG_TO_DOUBLE_COEFF;
     }
 
     /**
