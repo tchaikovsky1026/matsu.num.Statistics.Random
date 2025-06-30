db6a737ca329a7ee524ffd745279ca614e892555
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-06-30 17:52:29 +0900

    add: 浮動小数点数混合分布乱数のインターフェース定義

---
+++ src/matsu/num/statistics/random/mix/FloatingMixtureRnd.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/*
+ * 2025.6.30
+ */
+package matsu.num.statistics.random.mix;
+
+import matsu.num.statistics.random.FloatingRandomGenerator;
+
+/**
+ * <p>
+ * 浮動小数点数により値が表現された,
+ * 連続確率分布の混合モデルによる乱数発生器を表現する.
+ * </p>
+ * 
+ * <p>
+ * 乱数の生成は,
+ * {@link FloatingRandomGenerator#nextRandom(matsu.num.statistics.random.BaseRandom)}
+ * によって行われる. <br>
+ * {@link matsu.num.statistics.random.Rnd} のインターフェース説明のとおり,
+ * 実装のインスタンスはイミュータブルであり,
+ * (乱数であることを除いて) 関数的に振る舞う.
+ * </p>
+ * 
+ * @implSpec
+ *               このインターフェースは実装を隠ぺいして型を公開するためのものである. <br>
+ *               モジュール外で継承・実装してはいけない.
+ * 
+ * @author Matsuura Y.
+ */
+public interface FloatingMixtureRnd extends FloatingRandomGenerator {
+
+}
