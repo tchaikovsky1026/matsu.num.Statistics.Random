80536a584dd5fe8d2a53ca5ea28cde34feb27ffd
Matsuura Y. <88224610+tchaikovsky1026@users.noreply.github.com>
2025-06-30 14:59:08 +0900

    add: 混合分布パッケージ

---
+++ src/matsu/num/statistics/random/mix/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright © 2025 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+/**
+ * 混合分布モデルに従う乱数を扱うパッケージ.
+ */
+package matsu.num.statistics.random.mix;
