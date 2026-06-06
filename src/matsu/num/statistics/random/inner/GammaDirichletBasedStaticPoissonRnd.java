6d8a67f5e42078bfb3c2165fe89806bfe524e086
Matsu <88224610+tchaikovsky1026@users.noreply.github.com>
2026-06-07 00:19:58 +0900

    Feature/issue-98 InnerStaticPoissonRndとその実装の用意 (#100)
    * feat,test: InnerStaticPoissonRndを用意し,テストを整備
    
    * feat,test: GammaDirichletBasedStaticPoissonRndの実装,テスト通過
    
    * modify,test: update DirichletBasedStaticBinomialRnd,Test

---
+++ src/matsu/num/statistics/random/inner/GammaDirichletBasedStaticPoissonRnd.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright © 2026 Matsuura Y.
+ * 
+ * This software is released under the MIT License.
+ * http://opensource.org/licenses/mit-license.php
+ */
+
+/*
+ * 2026.6.6
+ */
+package matsu.num.statistics.random.inner;
+
+import java.util.Objects;
+
+import matsu.num.statistics.random.BaseRandom;
+import matsu.num.statistics.random.StaticGammaRnd;
+import matsu.num.statistics.random.base.LazyParameterlessRndFactory;
+import matsu.num.statistics.random.lib.Exponentiation;
+
+/**
+ * Gamma and Dirichlet 分布乱数を使用した, Poisson 分布に従う乱数発生を扱う. <br>
+ * Dirichlet 分布乱数は内部的にガンマ乱数を使用する.
+ * 
+ * @author Matsuura Y.
+ */
+public final class GammaDirichletBasedStaticPoissonRnd implements InnerStaticPoissonRnd {
+
+    /** Basic な方法を採用する場合の, 試行回数の最大値 */
+    private static final double MAX_TRIAL_BY_BASIC_METHOD = 16d;
+
+    private final Exponentiation exponentiation;
+    private final StaticGammaRnd staticGammaRnd;
+    private final InnerStaticBinomialRnd staticBinomialRnd;
+
+    /**
+     * 非公開コンストラクタ.
+     * 
+     * 引数チェックしていない.
+     */
+    private GammaDirichletBasedStaticPoissonRnd(
+            Exponentiation exponentiation,
+            StaticGammaRnd.Factory staticGammaRndFactory,
+            InnerStaticBinomialRnd.Factory staticBinomialRndFactory) {
+        super();
+        this.exponentiation = exponentiation;
+        this.staticGammaRnd = staticGammaRndFactory.instance();
+        this.staticBinomialRnd = staticBinomialRndFactory.instance();
+    }
+
+    @Override
+    public int next(double lambda, BaseRandom random) {
+        Objects.requireNonNull(random);
+        if (!InnerStaticPoissonRnd.acceptsParameter(lambda)) {
+            throw new IllegalArgumentException(
+                    String.format(
+                            "Illegal parameter: lambda = %s", lambda));
+        }
+
+        /*
+         * lambda が小さいとき, basic な方法で乱数生成: randBasic.
+         * lambda が大きいときは以下.
+         * 
+         * m >= 1 なる整数 m を定める.
+         * z: rand with sGamma(m)
+         * z > lambda なら, return Bin(m-1,lambda/z)
+         * z <= lambda なら, m + Poi(lambda-z)
+         */
+
+        int shift = 0;
+        // lambda, shift を更新しながら, 再帰的に区間減少を行う.
+        while (true) {
+            if (lambda <= MAX_TRIAL_BY_BASIC_METHOD) {
+                return shift + randBasic(lambda, random);
+            }
+
+            int m = ((int) lambda) >> 1; // m approx lambda/2
+            double z = staticGammaRnd.nextRandom(random, m);
+            // 極端な場合はやり直し
+            if (z < 1E-200 || z > 1E+200) {
+                continue;
+            }
+            if (z > lambda) {
+                return shift + staticBinomialRnd.next(m - 1, lambda / z, random);
+            }
+
+            // z <= lambda
+            shift += m;
+            lambda -= z;
+        }
+    }
+
+    /** 素朴な方法により二項乱数を生成する. */
+    private int randBasic(double lambda, BaseRandom random) {
+
+        /*
+         * sUni に従う U0,U1,... を発生.
+         * U0*U1*...*Uk <= exp(-lambda) となる最小の k を返す.
+         */
+
+        double exp_mLambda = exponentiation.exp(-lambda);
+        int k = 0;
+        double v = 1d;
+        while (true) {
+            v *= random.nextDouble();
+            if (v <= exp_mLambda) {
+                return k;
+            }
+            k++;
+        }
+    }
+
+    /** このインスタンスの文字列表現を返す. */
+    @Override
+    public String toString() {
+        return "InnerStaticPoissonRnd";
+    }
+
+    /**
+     * {@link GammaDirichletBasedStaticPoissonRnd} のファクトリを生成する.
+     * 
+     * @param exponentiation 指数関数の計算
+     * @param staticGammaRndFactory staticガンマ乱数生成器のファクトリ
+     * @param staticBinomialRndFactory static二項乱数生成器のファクトリ
+     * @return このクラスのファクトリ
+     * @throws NullPointerException 引数に null を含む場合
+     */
+    public static InnerStaticPoissonRnd.Factory createFactory(
+            Exponentiation exponentiation,
+            StaticGammaRnd.Factory staticGammaRndFactory,
+            InnerStaticBinomialRnd.Factory staticBinomialRndFactory) {
+        return new Factory(
+                Objects.requireNonNull(exponentiation),
+                Objects.requireNonNull(staticGammaRndFactory),
+                Objects.requireNonNull(staticBinomialRndFactory));
+    }
+
+    /** 非公開ファクトリの実装. */
+    private static final class Factory extends
+            LazyParameterlessRndFactory<InnerStaticPoissonRnd>
+            implements InnerStaticPoissonRnd.Factory {
+
+        /**
+         * 非公開コンストラクタ.
+         * 
+         * 引数チェックしていない.
+         */
+        Factory(Exponentiation exponentiation,
+                StaticGammaRnd.Factory staticGammaRndFactory,
+                InnerStaticBinomialRnd.Factory staticBinomialRndFactory) {
+            super(
+                    () -> new GammaDirichletBasedStaticPoissonRnd(
+                            exponentiation, staticGammaRndFactory, staticBinomialRndFactory),
+                    "InnerStaticPoissonRnd.Factory");
+        }
+    }
+}
